94e66ae2c484665fa13454f4e6ffc9340ab0579e
astashov <wxllfwlr@gmail.com>
2024-10-10 13:56:25 +0300

    Add comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_10_08_180034) do
+ActiveRecord::Schema[7.2].define(version: 2024_10_10_094318) do
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_comments_on_event_id"
+  end
+
   create_table "events", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "comments", "events"
 end
